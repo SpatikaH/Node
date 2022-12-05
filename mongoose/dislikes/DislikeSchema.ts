f26efc95495020f0bf265e99a4c05290c2491cce
Spatika Himanth <spatikahimanth@Spatikas-MacBook-Air.local>
2022-12-04 19:08:10 -0500

    Update Dao and Controller

--- mongoose/dislikes/DislikeSchema.ts
+++ mongoose/dislikes/DislikeSchema.ts
@@ -6,11 +6,10 @@
  import Dislike from "../../models/dislikes/Dislike";
 
  /**
-  * @typedef DislikeSchema Represents likes schema in mongoose
-  * @property {Tuit} tuit Tuit being liked
-  * @property {User} likedBy User liking the tuit
+  * @typedef DislikeSchema Represents dislikes schema in mongoose
+  * @property {Tuit} tuit Tuit being disliked
+  * @property {User} likedBy User disliking the tuit
   */
-
   const DislikeSchema = new mongoose.Schema<Dislike>({
       tuit: {type: Schema.Types.ObjectId, ref: "TuitModel"},
       dislikedBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
